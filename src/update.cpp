d99a9e0a66a1f73e727b1ee73186977b68b17b91
connor <connorharris427@gmail.com>
2023-12-18 15:29:11 -0500

    Maybe Now

--- src/update.cpp
+++ src/update.cpp
@@ -1,115 +1,111 @@
-#include <cstdio>
-#include <cstdlib>
 #include <iostream>
+#include <fstream>
+#include <sstream>
 #include <stdexcept>
+#include <string>
+#include <filesystem>
+
+namespace fs = std::filesystem;
 
 std::string getTag() {
-  std::string osName = "unknown";
+    std::string osName = "unknown";
 #if defined(_WIN32)
-  osName = "windows";
+    osName = "windows";
 #elif defined(__linux__)
-  osName = "linux";
+    osName = "linux";
 #elif defined(__APPLE__)
-  osName = "darwin";
+    osName = "darwin";
 #endif
 
-  if (osName == "linux" || osName == "darwin") {
-    try {
-      std::string result;
-      FILE *pipe = popen("curl -s "
-                         "'https://api.github.com/repos/TheDevConnor/"
-                         "Zura-Transpiled/releases'",
-                         "r");
-      if (!pipe) {
-        throw std::runtime_error("popen() failed");
-      }
+    if (osName == "linux" || osName == "darwin") {
+        try {
+            std::ostringstream resultStream;
+            std::string command = "curl -s 'https://api.github.com/repos/TheDevConnor/Zura-Transpiled/releases'";
+            std::FILE *pipe = popen(command.c_str(), "r");
+            if (!pipe) {
+                throw std::runtime_error("popen() failed");
+            }
 
-      char buffer[128];
-      while (!feof(pipe)) {
-        if (fgets(buffer, 128, pipe) != nullptr) {
-          result += buffer;
+            char buffer[128];
+            while (!std::feof(pipe)) {
+                if (std::fgets(buffer, 128, pipe) != nullptr) {
+                    resultStream << buffer;
+                }
+            }
+
+            pclose(pipe);
+
+            std::string result = resultStream.str();
+            std::string tag = result.substr(result.find("tag_name") + 11);
+            tag = tag.substr(0, tag.find(",") - 1);
+            tag = tag.substr(1);
+
+            return tag;
+        } catch (const std::exception &e) {
+            std::cerr << "Error: " << e.what() << std::endl;
+            return "";
         }
-      }
+    } else {
+        try {
+            std::ostringstream resultStream;
+            std::string command = "powershell -Command \"(Invoke-RestMethod -Uri 'https://api.github.com/repos/TheDevConnor/Zura-Transpiled/releases').tag_name\"";
+            std::FILE *pipe = popen(command.c_str(), "r");
+            if (!pipe) {
+                throw std::runtime_error("popen() failed");
+            }
 
-      pclose(pipe);
+            char buffer[128];
+            while (!std::feof(pipe)) {
+                if (std::fgets(buffer, 128, pipe) != nullptr) {
+                    resultStream << buffer;
+                }
+            }
 
-      std::string tag = result.substr(result.find("tag_name") + 11);
-      tag = tag.substr(0, tag.find(",") - 1);
-      tag = tag.substr(1);
+            pclose(pipe);
 
-      return tag;
-    } catch (const std::exception &e) {
-      std::cerr << "Error: " << e.what() << std::endl;
-      return "";
-    }
-  } else {
-    try {
-      std::string result;
-      std::string command = "powershell -Command \"(Invoke-RestMethod -Uri "
-                            "'https://api.github.com/repos/TheDevConnor/"
-                            "Zura-Transpiled/releases').tag_name\"";
-      FILE *pipe = popen(command.c_str(), "r");
-      if (!pipe) {
-        throw std::runtime_error("popen() failed");
-      }
+            std::string result = resultStream.str();
+            std::string tag = result.substr(result.find("tag_name") + 11);
+            tag = tag.substr(0, tag.find(",") - 1);
+            tag = tag.substr(1);
 
-      char buffer[128];
-      while (!feof(pipe)) {
-        if (fgets(buffer, 128, pipe) != nullptr) {
-          result += buffer;
+            return tag;
+        } catch (const std::exception &e) {
+            std::cerr << "Error: " << e.what() << std::endl;
+            return "";
         }
-      }
-
-      pclose(pipe);
-
-      std::string tag = result.substr(result.find("tag_name") + 11);
-      tag = tag.substr(0, tag.find(",") - 1);
-      tag = tag.substr(1);
-
-      return tag;
-    } catch (const std::exception &e) {
-      std::cerr << "Error: " << e.what() << std::endl;
-      return "";
     }
-    return "";
-  }
 }
 
 void installer() {
-  std::string osName = "unknown";
+    std::string osName = "unknown";
 #if defined(_WIN32)
-  osName = "windows";
+    osName = "windows";
 #elif defined(__linux__)
-  osName = "linux";
+    osName = "linux";
 #elif defined(__APPLE__)
-  osName = "darwin";
+    osName = "darwin";
 #endif
 
-  std::string tag = getTag();
+    std::string tag = getTag();
 
-  if (osName == "windows") {
-    std::string url =
-        "https://github.com/TheDevConnor/Zura-Transpiled/releases/download/" +
-        tag + "/zura.exe";
-    std::string command =
-        "powershell -Command \"Invoke-WebRequest " + url + "\"";
-    try {
-      system(command.c_str());
-      system("move zura.exe C:\\Windows\\System32");
-    } catch (const std::exception &e) {
-      std::cerr << "Error running command: " << e.what() << std::endl;
+    if (osName == "windows") {
+        std::string url = "https://github.com/TheDevConnor/Zura-Transpiled/releases/download/" + tag + "/zura.exe";
+        std::string command = "powershell -Command \"Invoke-WebRequest " + url + "\"";
+        try {
+            system(command.c_str());
+            fs::rename("zura.exe", "C:\\Windows\\System32\\zura.exe");
+        } catch (const std::exception &e) {
+            std::cerr << "Error running command: " << e.what() << std::endl;
+        }
+    } else if (osName == "linux" || osName == "darwin") {
+        std::string url = "https://github.com/TheDevConnor/Zura-Transpiled/releases/download/" + tag + "/zura";
+        std::string command = "wget " + url;
+        try {
+            system(command.c_str());
+            system("chmod +x zura");
+            system("sudo mv zura /usr/local/bin/zura");
+        } catch (const std::exception &e) {
+            std::cerr << "Error running command: " << e.what() << std::endl;
+        }
     }
-  } else if (osName == "linux" || osName == "darwin") {
-    std::string url =
-        "https://github.com/TheDevConnor/Zura-Transpiled/releases/download/" +
-        tag + "/zura";
-    std::string command = "wget " + url;
-    try {
-      system(command.c_str());
-      system("chmod +x zura");
-      system("sudo mv zura /usr/local/bin");
-    } catch (const std::exception &e) {
-      std::cerr << "Error running command: " << e.what() << std::endl;
-    }
-  }
 }
