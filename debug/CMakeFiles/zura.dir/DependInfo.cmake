cd122fcb6baacec3b305af26f13cb5da04c3459b
bearfm <77757734+bearfm@users.noreply.github.com>
2024-06-20 01:16:51 -0700

    gurb

---
+++ debug/CMakeFiles/zura.dir/DependInfo.cmake
@@ -0,0 +1,30 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/bear/zura-transpiled/src/helper/error/error.cpp" "CMakeFiles/zura.dir/src/helper/error/error.cpp.o" "gcc" "CMakeFiles/zura.dir/src/helper/error/error.cpp.o.d"
+  "/home/bear/zura-transpiled/src/helper/flags.cpp" "CMakeFiles/zura.dir/src/helper/flags.cpp.o" "gcc" "CMakeFiles/zura.dir/src/helper/flags.cpp.o.d"
+  "/home/bear/zura-transpiled/src/lexer/lexer.cpp" "CMakeFiles/zura.dir/src/lexer/lexer.cpp.o" "gcc" "CMakeFiles/zura.dir/src/lexer/lexer.cpp.o.d"
+  "/home/bear/zura-transpiled/src/main.cpp" "CMakeFiles/zura.dir/src/main.cpp.o" "gcc" "CMakeFiles/zura.dir/src/main.cpp.o.d"
+  "/home/bear/zura-transpiled/src/parser/expr.cpp" "CMakeFiles/zura.dir/src/parser/expr.cpp.o" "gcc" "CMakeFiles/zura.dir/src/parser/expr.cpp.o.d"
+  "/home/bear/zura-transpiled/src/parser/map.cpp" "CMakeFiles/zura.dir/src/parser/map.cpp.o" "gcc" "CMakeFiles/zura.dir/src/parser/map.cpp.o.d"
+  "/home/bear/zura-transpiled/src/parser/parser.cpp" "CMakeFiles/zura.dir/src/parser/parser.cpp.o" "gcc" "CMakeFiles/zura.dir/src/parser/parser.cpp.o.d"
+  "/home/bear/zura-transpiled/src/parser/stmt.cpp" "CMakeFiles/zura.dir/src/parser/stmt.cpp.o" "gcc" "CMakeFiles/zura.dir/src/parser/stmt.cpp.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
