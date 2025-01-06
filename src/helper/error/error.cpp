58f373e7044a6cd78c625a80f6dea965a30fe0f6
connor <connorharris427@gmail.com>
2025-01-06 01:07:51 -0500

    made the errors nicer

--- src/helper/error/error.cpp
+++ src/helper/error/error.cpp
@@ -25,8 +25,9 @@ std::string ErrorClass::printLine(int line, const char *start) {
 std::string
 ErrorClass::formatLineWithTokens(int line, int pos,
                                  const std::vector<Lexer::Token> &tokens,
-                                 bool highlightPos = false) {
+                                 bool highlightPos = false, bool getLastToken) {
   std::string formattedLine = lineNumber(line) + std::to_string(line) + " | ";
+  const Lexer::Token *lastToken = nullptr;
   int errorPos = formattedLine.size();
   ErrorPos = 0;
 
@@ -34,12 +35,18 @@ ErrorClass::formatLineWithTokens(int line, int pos,
     if (tk.line != line) continue;
 
     if (highlightPos && tk.column == pos) {
-      formattedLine += col.color("_", Color::RED, false, true) + " " + tk.value + " ";
-      ErrorPos = errorPos;
+      if (getLastToken && lastToken) {
+        formattedLine += col.color(lastToken->value, Color::RED, false, true) + " ";
+        errorPos += errorPos + lastToken->value.size() + 1;
+      } else {
+        formattedLine += col.color("_", Color::RED, false, true) + col.color(tk.value, Color::RED, false, true) + " ";
+        ErrorPos = errorPos; 
+      }
     } else {
       formattedLine += tk.value + " ";
     }
 
+    lastToken = &tk;
     errorPos += tk.value.size() + 1;
   }
 
@@ -49,9 +56,9 @@ ErrorClass::formatLineWithTokens(int line, int pos,
 
 std::string ErrorClass::currentLine(int line, int pos, Lexer &lexer,
                                     bool isParser, bool isTypeError,
-                                    const std::vector<Lexer::Token> &tokens) {
-  if (isParser || isTypeError) {
-    return formatLineWithTokens(line, pos, tokens, isParser);
+                                    const std::vector<Lexer::Token> &tokens, bool getLastToken) {
+  if (isParser || isTypeError || getLastToken) {
+    return formatLineWithTokens(line, pos, tokens, isParser, getLastToken);
   }
 
   const char *start = lexer.lineStart(line);
@@ -81,9 +88,7 @@ std::string ErrorClass::error(int line, int pos, const std::string &msg,
                                    : col.color(errorType, Color::MAGENTA, true, true)) + ": ";
   // check if the msg begins with 'No value found for key'
   if (msg.find("No value found for key") != std::string::npos) {
-    line_error += msg + "\n";
-    errors[line] = line_error;
-    return line_error; // return early so we don't print lines
+    return line_error; // no need to print the message
   } else {
     line_error += msg + "\n";
   }
@@ -94,41 +99,57 @@ std::string ErrorClass::error(int line, int pos, const std::string &msg,
     return line_error;
   }
 
+  if (isParser) {
+    if (msg.find("Expected a SEMICOLON") == 0) {
+        line_error += currentLine(line - 1, pos, lexer, isParser, isTypeError, tokens, true);
+        line_error += " ↳ " + col.color("NOTE", Color::BLUE) + ": If the line above is empty, it's possible that the error is on the line above.\n";
+    } else {
+        line_error += currentLine(line, pos, lexer, isParser, isTypeError, tokens);
+        line_error += std::string(ErrorPos, ' ') + col.color("^", Color::RED, false, true) + "\n";
+    }
+
+    if (errors.find(line) == errors.end()) {
+        errors[line] = line_error; // Add error if not already present
+    }
+    return line_error;
+  }
+
   if (isTypeError) {
     if (!note.empty()) {
       line_error += " ↳ " + col.color("NOTE", Color::BLUE) + ": " + note + "\n";
-      typeErros.push_back(line_error);
+      errors[line] = line_error;
       return line_error;
     }
-    line_error += (line > 1) ? currentLine(line - 1, 0, lexer, isParser, isTypeError, tokens)
-                             : "";
-    line_error += currentLine(line, pos, lexer, isParser, isTypeError, tokens);
-    line_error += currentLine(line + 1, 0, lexer, isParser, isTypeError, tokens);
-    typeErros.push_back(line_error);
+
+    line_error += (line > 0) ? currentLine(line - 1, pos, lexer, isParser, isTypeError, tokens) : "";
+    line_error += col.color(currentLine(line, pos, lexer, isParser, isTypeError, tokens), Color::GRAY, false, true);
+    line_error += currentLine(line + 1, pos, lexer, isParser, isTypeError, tokens);
+    errors[line] = line_error;
     return line_error;
   }
 
   if (isGeneration) {
+    if (!note.empty()) {
+      line_error += " ↳ " + col.color("NOTE", Color::BLUE) + ": " + note + "\n";
+    }
+    line_error += currentLine(line, pos, lexer, isParser, isTypeError, tokens);
+    line_error += std::string(pos + 5, ' ') + col.color("^", Color::RED, false, true) + "\n";
     errors[line] = line_error;
     return line_error;
   }
 
   if (!note.empty()) line_error += " ↳ " + note + "\n";
 
-  line_error += (line > 1) ? currentLine(line - 1, 0, lexer, isParser, isTypeError, tokens)
-                           : "";
+  // if we get here, it's a lexer error
   line_error += currentLine(line, pos, lexer, isParser, isTypeError, tokens);
-  line_error += std::string(ErrorPos, ' ') + col.color("^", Color::RED, false, true) + "\n";
-  line_error += currentLine(line + 1, 0, lexer, isParser, isTypeError, tokens);
+  line_error += std::string(pos + 5, ' ') + col.color("^", Color::RED, false, true) + "\n";
 
-  if (errors.find(line) == errors.end()) {
-    errors[line] = line_error;
-  }
+  if (errors.find(line) == errors.end()) errors[line] = line_error;
   return line_error;
 }
 
 void ErrorClass::printError() {
-  if (!errors.empty() || !typeErros.empty()) {
+  if (!errors.empty()) {
     if (!errors.empty()) {
       std::cout << "\r\033[2K"; // Clear the line and move to start -- overwrite previous garbage
       std::cout << "Total number of Errors: "
@@ -137,16 +158,6 @@ void ErrorClass::printError() {
                 << std::endl;
       for (const std::pair<int, std::string>& errorPair : errors) {
         std::cout << errorPair.second << std::endl;
-      }
-    }
-    if (!typeErros.empty()) {
-      std::cout << "\033[2K"; // Clear the line
-      std::cout << "Total number of Type Errors: "
-                << col.color(std::to_string(typeErros.size()), Color::RED, true,
-                             false)
-                << std::endl;
-      for (const std::string &error : typeErros) {
-        std::cout << error << std::endl;
       }
     }
     Exit(ExitValue::_ERROR);
--- src/helper/error/error.hpp
+++ src/helper/error/error.hpp
@@ -8,16 +8,15 @@
 #include <vector>
 
 namespace ErrorClass {
-inline static std::unordered_map<int, std::string> errors;
-inline static std::vector<std::string> typeErros;
+inline static std::unordered_map<int, std::string> errors = {};
 inline int ErrorPos = 0;
 
 std::string formatLineWithTokens(int line, int pos,
                                  const std::vector<Lexer::Token> &tokens,
-                                 bool highlightPos);
+                                 bool highlightPos, bool getLastToken = false);
 std::string currentLine(int line, int pos, Lexer &lexer, bool isParser,
                         bool isTypeError,
-                        const std::vector<Lexer::Token> &tokens);
+                        const std::vector<Lexer::Token> &tokens, bool getLastToken = false);
 std::string error(int line, int pos, const std::string &msg,
                   const std::string &note, const std::string &errorType,
                   const std::string &filename, Lexer &lexer,
